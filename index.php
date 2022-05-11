b905930bb9b8f8f4fac2b59adecf180de10df7c3
pvlst77 <pvlst77@gmail.com>
2022-05-11 10:29:01 +0300

    secondtask

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ $user_name = ''; // укажите здесь ваше имя
                     <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
+                            <span class="lot__amount">fgndfgndf</span>
                             <span class="lot__cost">цена<b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
