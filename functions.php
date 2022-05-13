195c2759a88b3e71015f687e71d865f051026adc
pvlst77 <pvlst77@gmail.com>
2022-05-13 11:25:51 +0300

    Задание №5

---
+++ functions.php
@@ -0,0 +1,110 @@
+<?php
+$is_auth = rand(0, 1);
+
+function sum_format(int $cost){
+    $cost=ceil($cost);
+    if($cost>=1000){
+        $cost = number_format($cost,0,"."," ");
+    }
+    return '<span class="lot__cost">'.$cost.'<b class="rub">₽</b></span>';
+}
+
+$user_name = 'hedonist'; // укажите здесь ваше имя
+$categories = [
+    [
+        'eng' => 'boards',
+        'rus' => 'Доски и лыжи',
+    ],
+    [
+        'eng' => 'attachment',
+        'rus' => 'Крепления',
+    ],
+    [
+        'eng' => 'boots',
+        'rus' => 'Ботинки',
+    ],
+    [
+        'eng' => 'clothing',
+        'rus' => 'Одежда',
+    ],
+    [
+        'eng' => 'tools',
+        'rus' => 'Инструменты',
+    ],
+    [
+        'eng' => 'other',
+        'rus' => 'Разное',
+    ],
+];
+$announcements = [
+    [
+        'name' => '2014 Rossignol District Snowboard',
+        'category' => 'Доски и лыжи',
+        'cost' => '10990',
+        'url' => 'img/lot-1.jpg'
+    ],
+    [
+        'name' => 'DC Ply Mens 2016/2017 Snowboard',
+        'category' => 'Доски и лыжи',
+        'cost' => '159999',
+        'url' => 'img/lot-2.jpg'
+    ],
+    [
+        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'category' => 'Крепления',
+        'cost' => '8000',
+        'url' => 'img/lot-3.jpg'
+    ],
+    [
+        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'category' => 'Ботинки',
+        'cost' => '10999',
+        'url' => 'img/lot-4.jpg'
+    ],
+    [
+        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'category' => 'Одежда',
+        'cost' => '7500',
+        'url' => 'img/lot-5.jpg'
+    ],
+    [
+        'name' => 'Маска Oakley Canopy',
+        'category' => 'Разное',
+        'cost' => '5400',
+        'url' => 'img/lot-6.jpg'
+    ],
+];
+function sub_format ($number)
+    {
+        $number = ceil($number);
+        if($number<1000)
+        {
+            $result = $number;
+        }
+        else
+        {
+            $result = number_format($number,0,","," ");
+        }
+        return $result.'<b class="rub">p</b>';
+    }
+
+    function timer()
+    {
+        $now = new DateTime('now');
+        $nextdaynight = new DateTime('24:00');
+        $interval = $now->diff($nextdaynight);
+        return $interval->format('%h:%i');
+    }
+    
+    function include_template($name, $data) {
+        $name = 'templates/' . $name;
+        $result = '';
+        if (!file_exists($name)) {
+            return $result;
+        }
+        ob_start();
+        extract($data);
+        require($name);
+        $result = ob_get_clean();
+        return $result;
+    }
---
+++ templates/index.php
@@ -0,0 +1,47 @@
+<section class="promo">
+        <h2 class="promo__title">Нужен стафф для катки?</h2>
+        <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
+        <ul class="promo__list">
+            <!--заполните этот список из массива категорий-->
+
+
+            <?php
+            foreach($categories as $a) {
+                ?><li class="promo__item promo__item--<?=$a['eng']?>">
+                <a class="promo__link" href="pages/all-lots.html"><?=$a['rus']?></a>
+                </li><?php
+            }
+            ?>
+        </ul>
+    </section>
+    <section class="lots">
+        <div class="lots__header">
+            <h2>Открытые лоты</h2>
+        </div>
+        <ul class="lots__list">
+            <!--заполните этот список из массива с товарами-->
+
+
+            <?php
+            foreach ($announcements as $a){
+            ?><li class="lots__item lot">
+                <div class="lot__image">
+                    <img src="<?=$a['url']?>" width="350" height="260" alt="">
+                </div>
+                <div class="lot__info">
+                    <span class="lot__category"><?=$a['category']?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$a['name']?></a></h3>
+                    <div class="lot__state">
+                        <div class="lot__rate">
+                            <span class="lot__amount">Стартовая Цена</span>
+                            <span class="lot__cost"><?=sub_format($a["cost"])?></span>
+                        </div>
+                        <div class="lot__timer timer">
+                        <?=timer()?>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <?php }?>
+        </ul>
+    </section>
